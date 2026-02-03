17be851f1b4f366b015e867c42c310d3d8a694b9
Doppp <eghhhh.github@gmail.com>
2026-02-03 16:35:35 +0800

    chore: bootstrap rails app + postgres + solidqueue

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,6 +21,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
   config.cache_store = :null_store
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Render exception templates for rescuable exceptions and raise for other exceptions.
   config.action_dispatch.show_exceptions = :rescuable
